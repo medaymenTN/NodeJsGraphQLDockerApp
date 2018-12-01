33e98c255b07e1540b55f90dc56dbf0e4cb3fb9d
Cristian Malfesi <cristian.malfesi@endeev.com>
2018-12-01 09:02:02 -0300

    - Begin unify schema - adding auto schema and model and graphql structure

---
+++ graphql/types/autoType.js
@@ -0,0 +1,33 @@
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLNonNull = require('graphql').GraphQLNonNull;
+var GraphQLID = require('graphql').GraphQLID;
+var GraphQLString = require('graphql').GraphQLString;
+//var GraphQLSDateTime = require('graphql').GraphQLString;
+
+//TODO Add a new data type DATETIME to the graphql (CHM75)
+
+// Auto Type
+exports.autoType = new GraphQLObjectType({
+  name: 'autos',
+  fields:  () =>{
+    return {
+      id: {
+        type: new GraphQLNonNull(GraphQLID)
+      },
+      modelo: {
+        type: GraphQLString
+      },
+      patente: {
+        type: GraphQLString
+      },
+      color: {
+        type: GraphQLString
+      },
+      fechaAlta: {
+        type: GraphQLString
+      } 
+    }
+  }
+});
+
+
